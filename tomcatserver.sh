fcdae3a91f328cee40962bea4bec5ac51b01a4da
Y PHANI KUMAR <40165781+Yphanikumar5@users.noreply.github.com>
2018-09-28 12:31:38 +0530

    Update tomcatserver.sh

--- tomcatserver.sh
+++ tomcatserver.sh
@@ -29,10 +29,11 @@
 	fi	
 }
 	TomcatUninstall()
+	{
 	 cd /opt/
 	sudo rm -rf apache-tomcat-9.0.12
 	sudo rm -rf apache-tomcat-9.0.12.zip
-	
+	}
 	TomcatOutAccess()
 	{
 		cd /opt/apache-tomcat-9.0.12/webapps/host-manager/META-INF
